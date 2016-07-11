687a923f502eaa09f4e4b27ec0cc64d796773fa3
Joann Im <joann_im@hotmail.com>
2016-07-11 14:58:05 -0700

    Fix listCalendar function and made listCalendar, currentMonth, currentYear, and allDates a global variable  Please enter the commit message for your changes. Lines starting

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -1,25 +1,29 @@
 //create a function that takes the current month and the current year
-var listCalendar = function(month, year) {
+window.listCalendar = function(month, year) {
+  var monthString = month.toString();
+  var yearString = year.toString();
   var allDates = [];
+  console.log('month is now', month);
+  console.log('typeof month is', typeof month);
   var days;
   if (month === 2) {
     days = 28;
-  }
-  if (month === 4 || month === 6 || month === 9 || month === 11) {
-  	days = 30;
+  } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+    days = 30;
   } else {
-  	days = 31;
+    days = 31;
   }
 
-  for (var i = 1; i < days.length; i++) {
-  	var currentDay = month + '/' + i + '/' + year;
-    allDates.push({date: currentDay, effects: [], breakout: 'false'});
+  for (var i = 1; i <= days; i++) {
+    var currentDay = monthString + '/' + i + '/' + yearString;
+    allDates.push({ date: currentDay, effects: [], breakout: 'false' });
   }
   return allDates;
-}
+};
+
 
 var d = new Date();
-var currentMonth = d.getMonth();
-var currentYear = d.getFullYear().toString().slice(2);
+window.currentMonth = d.getMonth();
+window.currentYear = Number(d.getFullYear().toString().slice(2));
 
-window.startingData = listCalendar(currentMonth, currentYear);
+window.allDates = listCalendar(currentMonth, currentYear);
