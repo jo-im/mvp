c36a65793ff16be762475e19fa869cf5962da002
Joann Im <joann_im@hotmail.com>
2016-07-11 14:56:43 -0700

    Made App component into a class and made buttons that will allow users to go previous month and next month. Users can now go to the previous month.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,35 @@
-var App = () => (
-  <div>
-      Render App
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+  	super(props);
+
+  	this.state = {
+  	  daysOfMonth: this.props.allDates
+  	};
+  }
+
+  onPrevButtonClick() {
+  	console.log('PREVBUTTONCLICK HAS BEEN CLICKED');
+  	if (currentMonth === 1) {
+      currentMonth = 12
+      currentYear--;
+  	} else {
+  	  currentMonth--;
+  	}
+
+  	this.setState({
+   	  daysOfMonth: listCalendar(currentMonth, currentYear)
+  	})
+  }
+
+  render() {
+  	return (
+  	  <div>
+  	    <button onClick={this.onPrevButtonClick.bind(this)} type="button">Prev</button>
+  	    <button type="button">Next</button>
+  	    <ListOfDates allDates={this.state.daysOfMonth}/>
+  	  </div>
+  	)
+  }
+}
 
 window.App = App;
