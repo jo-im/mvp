dc45f3bd7dfccf99aa4f1e4c2b55e57d79351cd0
Joann Im <joann_im@hotmail.com>
2016-07-11 15:14:48 -0700

    Create a CauseTable component that adds a table of possible causes of skin problems

---
+++ src/components/CauseTable.jsx
@@ -0,0 +1,10 @@
+var CauseTable = (props) => (
+  <div>
+    <button type="button">Product</button>
+    <button type="button">Lack Of Sleep</button>
+    <button type="button">Poor Diet</button>
+    <button type="button">Stress</button>
+  </div>
+);
+
+window.CauseTable = CauseTable;
