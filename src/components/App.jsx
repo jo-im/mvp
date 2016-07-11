921d5c6fc5d5df71cb2f94ec365cd76215a55371
Joann Im <joann_im@hotmail.com>
2016-07-11 15:07:08 -0700

    Users can now go to the next month

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,11 +21,25 @@ class App extends React.Component {
   	})
   }
 
+  onNextButtonClick() {
+  	console.log('NEXTBUTTONCLICK HAS BEEN CLICKED');
+  	if (currentMonth === 12) {
+  	  currentMonth = 1;
+  	  currentYear++;
+  	} else {
+  	  currentMonth++;
+  	}
+
+  	this.setState({
+  	  daysOfMonth: listCalendar(currentMonth, currentYear)
+  	})
+  }
+
   render() {
   	return (
   	  <div>
   	    <button onClick={this.onPrevButtonClick.bind(this)} type="button">Prev</button>
-  	    <button type="button">Next</button>
+  	    <button onClick={this.onNextButtonClick.bind(this)} type="button">Next</button>
   	    <ListOfDates allDates={this.state.daysOfMonth}/>
   	  </div>
   	)
