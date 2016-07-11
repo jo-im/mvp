1bb431a3862eda4bcbc4b525833d6abe3bddcbc4
Joann Im <joann_im@hotmail.com>
2016-07-11 16:04:32 -0700

    Have each possible cause buttons to have an event handler called onCauseButtonClick

--- src/components/CauseTable.jsx
+++ src/components/CauseTable.jsx
@@ -1,9 +1,9 @@
 var CauseTable = (props) => (
   <div>
-    <button type="button">Product</button>
-    <button type="button">Lack Of Sleep</button>
-    <button type="button">Poor Diet</button>
-    <button type="button">Stress</button>
+    <button onClick={props.onCauseButtonClick} type="button">Product</button>
+    <button onClick={props.onCauseButtonClick} type="button">Lack Of Sleep</button>
+    <button onClick={props.onCauseButtonClick} type="button">Poor Diet</button>
+    <button onClick={props.onCauseButtonClick} type="button">Stress</button>
   </div>
 );
 
